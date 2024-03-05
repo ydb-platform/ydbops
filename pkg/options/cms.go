7eaa98f67469f2fe41996918063a26a978c1d1fc
Jorres Tarasov <jorres.tarasov@gmail.com>
2024-03-06 00:02:32 +0300

    fix: remove max\keep\force cms availability mode

--- pkg/options/cms.go
+++ pkg/options/cms.go
@@ -4,12 +4,6 @@ import (
 	"fmt"
 
 	"github.com/spf13/pflag"
-
-	"github.com/ydb-platform/ydb-ops/internal/util"
-)
-
-var (
-	CMSAvailabilityModes = []string{"max", "keep", "force"}
 )
 
 const (
@@ -20,14 +14,11 @@ const (
 )
 
 type CMS struct {
-	AvailabilityMode string
 	RetryWaitSeconds int
 	TimeoutSeconds int
 }
 
 func (cms *CMS) DefineFlags(fs *pflag.FlagSet) {
-	fs.StringVarP(&cms.AvailabilityMode, "cms-availability-mode", "", CMSDefaultAvailAbilityMode,
-		fmt.Sprintf("CMS Availability mode (%+v)", CMSAvailabilityModes))
 	fs.IntVarP(&cms.RetryWaitSeconds, "cms-wait-time-seconds", "", CMSDefaultRetryWaitTime,
 		"CMS retry time in seconds")
 	fs.IntVarP(&cms.TimeoutSeconds, "cms-timeout-seconds", "", CMSDefaultTimeoutSeconds,
@@ -35,9 +26,6 @@ func (cms *CMS) DefineFlags(fs *pflag.FlagSet) {
 }
 
 func (cms *CMS) Validate() error {
-	if !util.Contains(CMSAvailabilityModes, cms.AvailabilityMode) {
-		return fmt.Errorf("invalid availability mode specified: %v, use one of: %+v", cms.AvailabilityMode, CMSAvailabilityModes)
-	}
 	if cms.RetryWaitSeconds < 0 {
 		return fmt.Errorf("invalid value specified: %d", cms.RetryWaitSeconds)
 	}
