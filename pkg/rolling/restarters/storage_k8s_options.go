8574d6ffb2d2d4e29610f04a4a42fd224a3829ea
Jorres Tarasov <jorres.tarasov@gmail.com>
2024-03-19 01:05:01 +0300

    exp: remove --storage and --database flags

--- pkg/rolling/restarters/storage_k8s_options.go
+++ pkg/rolling/restarters/storage_k8s_options.go
@@ -1,26 +1,20 @@
 package restarters
 
 import (
-	"fmt"
 	"github.com/spf13/pflag"
 )
 
 type StorageK8sOpts struct {
 	k8sOpts
-	storage        string
 }
 
 func (o *StorageK8sOpts) DefineFlags(fs *pflag.FlagSet) {
-	fs.StringVar(&o.storage, "storage", "", "Storage resource name to restart")
 	o.k8sOpts.DefineFlags(fs)
 }
 
 func (o *StorageK8sOpts) Validate() error {
 	if err := o.k8sOpts.Validate(); err != nil {
 		return err
-	}
-	if o.storage == "" {
-		return fmt.Errorf("Please specify a non-empty --storage")
 	}
 	return nil
 }
