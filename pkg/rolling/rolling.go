7539b465e25cd303097aeeb7cdb025f98da58e2d
Jorres Tarasov <jorres.tarasov@gmail.com>
2024-06-11 14:22:20 +0200

    fix: broken build after rebasing parallel tenants

--- pkg/rolling/rolling.go
+++ pkg/rolling/rolling.go
@@ -16,6 +16,13 @@ import (
 	"github.com/ydb-platform/ydbops/pkg/rolling/restarters"
 )
 
+type NodeType int
+
+const (
+	NodeTypeStorage NodeType = 1
+	NodeTypeDynnode NodeType = 2
+)
+
 type Rolling struct {
 	cms       *client.Cms
 	discovery *client.Discovery
@@ -25,20 +32,16 @@ type Rolling struct {
 	opts      options.RestartOptions
 	restarter restarters.Restarter
 
-	// TODO jorres@: maybe turn this into a local `map`
-	// variable in `processActionGroupStates`
-	completedActions []*Ydb_Maintenance.ActionUid
+	nodeType NodeType
 }
 
 type state struct {
-	nodes                          map[uint32]*Ydb_Maintenance.Node
-	inactiveNodes                  map[uint32]*Ydb_Maintenance.Node
-	tenantNameToNodeIds            map[string][]uint32
-	retriesMadeForNode             map[uint32]int
-	tenants                        []string
-	userSID                        string
-	unreportedButFinishedActionIds []string
-	restartTaskUID                 string
+	nodes               map[uint32]*Ydb_Maintenance.Node
+	inactiveNodes       map[uint32]*Ydb_Maintenance.Node
+	tenantNameToNodeIds map[string][]uint32
+	retriesMadeForNode  map[uint32]int
+	tenants             []string
+	userSID             string
 }
 
 const (
@@ -49,6 +52,7 @@ func ExecuteRolling(
 	restartOpts options.RestartOptions,
 	logger *zap.SugaredLogger,
 	restarter restarters.Restarter,
+	nodeType NodeType,
 ) error {
 	cmsClient := client.GetCmsClient()
 	discoveryClient := client.GetDiscoveryClient()
@@ -59,6 +63,7 @@ func ExecuteRolling(
 		logger:    logger,
 		opts:      restartOpts,
 		restarter: restarter,
+		nodeType:  nodeType,
 	}
 
 	var err error
@@ -77,6 +82,20 @@ func ExecuteRolling(
 
 	logger.Info("Restart completed successfully")
 	return nil
+}
+
+type ProgressMessage struct {
+	err error
+
+	isExit bool
+
+	render func(r *zap.SugaredLogger)
+
+	// isTask   bool
+	// task     client.MaintenanceTask
+
+	// isResult bool
+	// result   client.MaintenanceTask
 }
 
 func (r *Rolling) DoRestart() error {
@@ -115,89 +134,184 @@ func (r *Rolling) DoRestart() error {
 		},
 	)
 
-	excludedNodes := 0
+	activeNodes := []*Ydb_Maintenance.Node{}
 	for _, node := range nodesToRestart {
 		if _, present := r.state.inactiveNodes[node.NodeId]; present {
-			excludedNodes++
 			r.logger.Warn(
 				"the node with nodeId: %v and host: %s is currently down and will be excluded from restart",
 				node.Host,
 				node.NodeId,
 			)
+		} else {
+			activeNodes = append(activeNodes, node)
 		}
 	}
 
-	if len(nodesToRestart)-excludedNodes == 0 {
+	if len(activeNodes) == 0 {
 		r.logger.Warn("There are no nodes that satisfy the specified filters")
 		return nil
 	}
 
-	taskParams := client.MaintenanceTaskParams{
-		TaskUID:          r.state.restartTaskUID,
-		AvailabilityMode: r.opts.GetAvailabilityMode(),
-		Duration:         r.opts.GetRestartDuration(),
-		ScopeType:        client.NodeScope,
-		Nodes:            nodesToRestart,
-	}
-	task, err := r.cms.CreateMaintenanceTask(taskParams)
-	if err != nil {
-		return fmt.Errorf("failed to create maintenance task: %w", err)
+	tasksParams := [][]client.MaintenanceTaskParams{}
+
+	if r.nodeType == NodeTypeStorage {
+		tasksParams = append(tasksParams, []client.MaintenanceTaskParams{})
+		tasksParams[0] = append(tasksParams[0], r.makeTaskParams(nodesToRestart))
+	} else {
+		for _, tenantNodeIds := range r.state.tenantNameToNodeIds {
+			curTenantTasks := []client.MaintenanceTaskParams{}
+			tasksParams = append(tasksParams)
+
+			cnt := 0
+			curTaskNodes := []*Ydb_Maintenance.Node{}
+			for _, nodeId := range tenantNodeIds {
+				cnt++
+				curTaskNodes = append(curTaskNodes, r.state.nodes[nodeId])
+				if cnt == 10 { // TODO r.opts.MaxPerTenant
+					curTenantTasks = append(curTenantTasks, r.makeTaskParams(curTaskNodes))
+					cnt = 0
+					curTaskNodes = []*Ydb_Maintenance.Node{}
+				}
+			}
+			if cnt > 0 {
+				curTenantTasks = append(curTenantTasks, r.makeTaskParams(curTaskNodes))
+			}
+
+			tasksParams = append(tasksParams, curTenantTasks)
+		}
 	}
 
-	return r.cmsWaitingLoop(task, len(nodesToRestart))
+	resultChannel := make(chan ProgressMessage, 1024)
+
+	wg := sync.WaitGroup{}
+	for _, sequentialTasks := range tasksParams {
+		wg.Add(1)
+		go func() {
+			r.cmsWaitingLoop(sequentialTasks, resultChannel)
+			wg.Done()
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		resultChannel <- ProgressMessage{
+			isExit: true,
+		}
+	}()
+
+	for {
+		msg := <-resultChannel
+		if msg.isExit {
+			return nil
+		} else if msg.err != nil {
+			return err
+		} else {
+			// TODO print
+			defaultDelay := time.Duration(r.opts.CMSQueryInterval) * time.Second
+			time.Sleep(defaultDelay)
+		}
+	}
+}
+
+// TODO
+// I think where I'm going should work.
+// I don't like the fact that I'm twisting the maintenance API in a way it was not supposed to be twisted.
+// E.g. you restart 10 nodes, 9 restart quickly, last restarts slowly, 9 'slots' are free and do nothing, although
+// you could have already taken 10 other nodes.
+//
+// Maybe I should challenge the fact that CMS just does not have the concept of an absolute limit, only a relative limit?
+// Who is at fault?
+// But it will still be possibly some good value even though the code looks like shit LOL
+// It just saddens me that
+// Maybe I can just create a task per node and continuously have this "pool" inside of one loop? Discuss implications of this
+// with ilya shakhov.
+//
+// Local improvement:
+// why having this ProgressChannel, if you can just allocate enough taskstates and combine the output together?
+// I bet it would be simpler
+
+func (r *Rolling) makeTaskParams(nodes []*Ydb_Maintenance.Node) client.MaintenanceTaskParams {
+	return client.MaintenanceTaskParams{
+		TaskUID:          RestartTaskPrefix + uuid.New().String(),
+		AvailabilityMode: r.opts.GetAvailabilityMode(),
+		Duration:         r.opts.GetRestartDuration(),
+		Nodes:            nodes,
+	}
 }
 
 func (r *Rolling) DoRestartPrevious() error {
 	return fmt.Errorf("--continue behavior not implemented yet")
 }
 
-func (r *Rolling) cmsWaitingLoop(task client.MaintenanceTask, totalNodes int) error {
-	var (
-		err          error
-		delay        time.Duration
-		taskID       = task.GetTaskUid()
-		defaultDelay = time.Duration(r.opts.CMSQueryInterval) * time.Second
-	)
+type TaskState struct {
+	m                               *sync.Mutex
+	unreportedButCompletedActionIds []*Ydb_Maintenance.ActionUid
+	restartedNodes                  int
+	retriesMadeForNode              map[uint32]int
+}
 
-	restartedNodes := 0
+func (r *Rolling) cmsWaitingLoop(tasksParams []client.MaintenanceTaskParams, resultChannel chan ProgressMessage) {
+	for _, taskParams := range tasksParams {
+		task, err := r.cms.CreateMaintenanceTask(taskParams)
+		if err != nil {
+			resultChannel <- ProgressMessage{
+				err: err,
+			}
+			return
+		}
 
-	r.logger.Infof("Maintenance task %v, processing loop started", taskID)
-	for {
-		delay = defaultDelay
+		var (
+			delay        time.Duration
+			taskID       = task.GetTaskUid()
+			defaultDelay = time.Duration(r.opts.CMSQueryInterval) * time.Second
+		)
 
-		if task != nil {
-			r.logTask(task)
+		taskState := &TaskState{
+			restartedNodes:                  0,
+			unreportedButCompletedActionIds: []*Ydb_Maintenance.ActionUid{},
+			retriesMadeForNode:              make(map[uint32]int),
+			m:                               new(sync.Mutex),
+		}
+
+		// r.logger.Infof("Maintenance task %v, processing loop started", taskID)
+		for {
+			delay = defaultDelay
+
+			// r.logTask(task)
+
+			resultChannel <- ProgressMessage{
+				task: task,
+			}
 
 			if task.GetRetryAfter() != nil {
 				retryTime := task.GetRetryAfter().AsTime()
-				r.logger.Debugf("Task has retry after attribute: %s", retryTime.Format(time.DateTime))
+				// r.logger.Debugf("Task has retry after attribute: %s", retryTime.Format(time.DateTime))
 
 				if retryDelay := retryTime.Sub(time.Now().UTC()); defaultDelay < retryDelay {
 					delay = defaultDelay
 				}
 			}
 
-			if completed := r.processActionGroupStates(task.GetActionGroupStates(), &restartedNodes); completed {
+			if completed := r.processActionGroupStates(task.GetActionGroupStates(), taskState); completed {
 				break
 			}
+
+			// r.logger.Infof("Wait next %s delay. Total node progress: %v out of %v", delay, restartedNodes, totalNodes)
+			time.Sleep(delay)
+
+			// r.logger.Infof("Refresh maintenance task with id: %s", taskID)
+			task, err = r.cms.RefreshMaintenanceTask(taskID)
+			// if err != nil {
+			// r.logger.Warnf("Failed to refresh maintenance task: %+v", err)
+			// }
 		}
 
-		r.logger.Infof("Wait next %s delay. Total node progress: %v out of %v", delay, restartedNodes, totalNodes)
-		time.Sleep(delay)
-
-		r.logger.Infof("Refresh maintenance task with id: %s", taskID)
-		task, err = r.cms.RefreshMaintenanceTask(taskID)
-		if err != nil {
-			r.logger.Warnf("Failed to refresh maintenance task: %+v", err)
-		}
+		// r.logger.Infof("Maintenance task processing loop completed")
 	}
-
-	r.logger.Infof("Maintenance task processing loop completed")
-	return nil
 }
 
-func (r *Rolling) processActionGroupStates(actions []*Ydb_Maintenance.ActionGroupStates, restartedNodes *int) bool {
-	r.logger.Debugf("Unfiltered ActionGroupStates: %v", actions)
+func (r *Rolling) processActionGroupStates(actions []*Ydb_Maintenance.ActionGroupStates, taskState *TaskState, progress chan ProgressMessage) bool {
+	// r.logger.Debugf("Unfiltered ActionGroupStates: %v", actions)
 	performed := collections.FilterBy(actions,
 		func(gs *Ydb_Maintenance.ActionGroupStates) bool {
 			return gs.ActionStates[0].Status == Ydb_Maintenance.ActionState_ACTION_STATUS_PERFORMED
@@ -205,16 +319,14 @@ func (r *Rolling) processActionGroupStates(actions []*Ydb_Maintenance.ActionGrou
 	)
 
 	if len(performed) == 0 {
-		r.logger.Info("No actions can be taken yet, waiting for CMS to move some actions to PERFORMED...")
+		// r.logger.Info("No actions can be taken yet, waiting for CMS to move some actions to PERFORMED...")
 		return false
 	}
 
-	r.logger.Infof("%d ActionGroupStates moved to PERFORMED, will restart now...", len(performed))
+	// r.logger.Infof("%d ActionGroupStates moved to PERFORMED, will restart now...", len(performed))
 
-	r.completedActions = []*Ydb_Maintenance.ActionUid{}
+	completedActions := []*Ydb_Maintenance.ActionUid{}
 	wg := new(sync.WaitGroup)
-
-	rollingStateMutex := new(sync.Mutex)
 
 	for _, gs := range performed {
 		var (
@@ -223,13 +335,13 @@ func (r *Rolling) processActionGroupStates(actions []*Ydb_Maintenance.ActionGrou
 			node = r.state.nodes[lock.Scope.GetNodeId()]
 		)
 
-		if collections.Contains(r.state.unreportedButFinishedActionIds, as.ActionUid.ActionId) {
-			r.completedActions = append(r.completedActions, as.ActionUid)
-			r.logger.Debugf(
-				"Node id %v already restarted, but CompleteAction failed on last iteration, "+
-					"so CMS does not know it is complete yet.",
-				node.NodeId,
-			)
+		if collections.Contains(taskState.unreportedButCompletedActionIds, as.ActionUid.ActionId) {
+			completedActions = append(completedActions, as.ActionUid)
+			// r.logger.Debugf(
+			// 	"Node id %v already restarted, but CompleteAction failed on last iteration, "+
+			// 		"so CMS does not know it is complete yet.",
+			// 	node.NodeId,
+			// )
 			continue
 		}
 
@@ -244,50 +356,65 @@ func (r *Rolling) processActionGroupStates(actions []*Ydb_Maintenance.ActionGrou
 
 			r.logger.Debugf("Restart node with id: %d", node.NodeId)
 			if err := r.restarter.RestartNode(node); err != nil {
-				rollingStateMutex.Lock()
-				retriesUntilNow := r.state.retriesMadeForNode[node.NodeId]
-				r.state.retriesMadeForNode[node.NodeId]++
-				rollingStateMutex.Unlock()
+				retriesUntilNow := taskState.getAndIncrement(node.NodeId)
 
-				r.logger.Warnf(
-					"Failed to restart node with id: %d, attempt number %v, because of: %s",
-					node.NodeId,
-					retriesUntilNow,
-					err.Error(),
-				)
+				// rollingStateMutex.Lock()
+				// retriesUntilNow := r.state.retriesMadeForNode[node.NodeId]
+				// r.state.retriesMadeForNode[node.NodeId]++
+				// rollingStateMutex.Unlock()
+
+				// r.logger.Warnf(
+				// 	"Failed to restart node with id: %d, attempt number %v, because of: %s",
+				// 	node.NodeId,
+				// 	retriesUntilNow,
+				// 	err.Error(),
+				// )
 
 				if retriesUntilNow+1 == r.opts.RestartRetryNumber {
-					r.atomicRememberComplete(rollingStateMutex, as.ActionUid, restartedNodes)
-					r.logger.Warnf("Failed to retry node %v specified number of times (%v)", node.NodeId, r.opts.RestartRetryNumber)
+					taskState.rememberComplete(as.ActionUid, completedActions)
+					// r.logger.Warnf("Failed to retry node %v specified number of times (%v)", node.NodeId, r.opts.RestartRetryNumber)
 				}
 			} else {
-				r.atomicRememberComplete(rollingStateMutex, as.ActionUid, restartedNodes)
-				r.logger.Debugf("Successfully restarted node with id: %d", node.NodeId)
+				taskState.rememberComplete(as.ActionUid, completedActions)
+				// r.logger.Debugf("Successfully restarted node with id: %d", node.NodeId)
 			}
 		}()
 	}
 
 	wg.Wait()
 
-	result, err := r.cms.CompleteAction(r.completedActions)
+	result, err := r.cms.CompleteAction(completedActions)
 	if err != nil {
 		r.logger.Warnf("Failed to complete action: %+v", err)
+		taskState.unreportedButCompletedActionIds = completedActions
 		return false
 	}
-	r.logCompleteResult(result)
-	r.state.unreportedButFinishedActionIds = []string{}
 
-	// completed when all actions marked as completed
+	progress <- ProgressMessage{
+		render: func(logger *zap.SugaredLogger) {
+			logCompleteResult(logger, result)
+		},
+	}
+
+	taskState.unreportedButCompletedActionIds = []*Ydb_Maintenance.ActionUid{}
+
 	return len(actions) == len(result.ActionStatuses)
 }
 
-func (r *Rolling) atomicRememberComplete(m *sync.Mutex, actionUID *Ydb_Maintenance.ActionUid, restartedNodes *int) {
-	m.Lock()
-	defer m.Unlock()
+func (t *TaskState) getAndIncrement(nodeId uint32) int {
+	t.m.Lock()
+	defer t.m.Unlock()
 
-	r.state.unreportedButFinishedActionIds = append(r.state.unreportedButFinishedActionIds, actionUID.ActionId)
-	r.completedActions = append(r.completedActions, actionUID)
-	(*restartedNodes)++
+	retries := t.retriesMadeForNode[nodeId]
+	t.retriesMadeForNode[nodeId]++
+	return retries
+}
+
+func (t *TaskState) rememberComplete(actionUid *Ydb_Maintenance.ActionUid, completedActions []*Ydb_Maintenance.ActionUid) {
+	t.m.Lock()
+	defer t.m.Unlock()
+
+	completedActions = append(completedActions, actionUid)
 }
 
 func (r *Rolling) populateTenantToNodesMapping(nodes []*Ydb_Maintenance.Node) map[string][]uint32 {
@@ -336,14 +463,12 @@ func (r *Rolling) prepareState() (*state, error) {
 	}
 
 	return &state{
-		tenantNameToNodeIds:            r.populateTenantToNodesMapping(activeNodes),
-		tenants:                        tenants,
-		userSID:                        userSID,
-		nodes:                          collections.ToMap(activeNodes, func(n *Ydb_Maintenance.Node) uint32 { return n.NodeId }),
-		inactiveNodes:                  collections.ToMap(inactiveNodes, func(n *Ydb_Maintenance.Node) uint32 { return n.NodeId }),
-		retriesMadeForNode:             make(map[uint32]int),
-		unreportedButFinishedActionIds: []string{},
-		restartTaskUID:                 RestartTaskPrefix + uuid.New().String(),
+		tenantNameToNodeIds: r.populateTenantToNodesMapping(activeNodes),
+		tenants:             tenants,
+		userSID:             userSID,
+		nodes:               collections.ToMap(activeNodes, func(n *Ydb_Maintenance.Node) uint32 { return n.NodeId }),
+		inactiveNodes:       collections.ToMap(inactiveNodes, func(n *Ydb_Maintenance.Node) uint32 { return n.NodeId }),
+		retriesMadeForNode:  make(map[uint32]int),
 	}, nil
 }
 
@@ -368,10 +493,10 @@ func (r *Rolling) logTask(task client.MaintenanceTask) {
 	r.logger.Debugf("Maintenance task result:\n%s", prettyprint.TaskToString(task))
 }
 
-func (r *Rolling) logCompleteResult(result *Ydb_Maintenance.ManageActionResult) {
+func logCompleteResult(logger *zap.SugaredLogger, result *Ydb_Maintenance.ManageActionResult) {
 	if result == nil {
 		return
 	}
 
-	r.logger.Debugf("Manage action result:\n%s", prettyprint.ResultToString(result))
+	logger.Debugf("Manage action result:\n%s", prettyprint.ResultToString(result))
 }
